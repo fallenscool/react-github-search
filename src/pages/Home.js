74f98e2332e65987c04d1bdfd4100aa069f4e284
Vladimir Spivachevsky <vladimirspivachevsky@n1ce.local>
2019-10-18 17:34:30 +0300

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, {useContext} from 'react';
+import {Search} from "../components/Search";
+import {Card} from "../components/Card";
+import {GithubContext} from "../context/github/githubContext";
+
+export const Home = () => {
+    const {loading, users} = useContext(GithubContext);
+
+    return (
+        <>
+            <Search/>
+            <div className="row">
+                {
+                    loading
+                        ? <p className="text-center">Загрузка...</p>
+                        : users.map(user => (
+                            <div className="col-sm-4 mb-4" key={user.id}>
+                                <Card user={user}/>
+                            </div>
+                        ))
+
+                }
+            </div>
+        </>
+    );
+};
---
+++ src/pages/Profile.js
@@ -0,0 +1,76 @@
+import React, {useContext, useEffect} from 'react';
+import {GithubContext} from "../context/github/githubContext";
+import {Link} from "react-router-dom";
+import {Repos} from "../components/Repos";
+
+export const Profile = ({match}) => {
+    const {getUser, getRepos, loading, user, repos} = useContext(GithubContext);
+    const urlName = match.params.name;
+
+    useEffect(() => {
+        getUser(urlName);
+        getRepos(urlName);
+        // eslint-disable-next-line
+    }, []);
+
+    if (loading) {
+        return <p className="text-center">Загрузка...</p>
+    }
+    const {
+        name, company, avatar_url,
+        location, bio, blog, login,
+        html_url, followers, public_repos,
+        public_gists, following
+    } = user;
+
+    return (
+        <>
+            <Link to={'/'} className="btn btn-link">На главную</Link>
+            <div className="card mb-3">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-3 text-center">
+                            <img
+                                src={avatar_url}
+                                alt={name}
+                                style={{width:'200px'}}
+                            />
+                            <h1>{name}</h1>
+                            {location && <p>Местоположение: {location}</p>}
+                        </div>
+                        <div className="col">
+                            {
+                                bio && <>
+                                    <h3>BIO</h3>
+                                    <p>{bio}</p>
+                                </>
+                            }
+                            <a
+                                href={html_url}
+                                className="btn btn-dark"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >Открыть профиль</a>
+                            <ul>
+                                {login && <li>
+                                    <strong>Username: </strong> {login}
+                                </li>}
+                                {company && <li>
+                                    <strong>Компания: </strong> {company}
+                                </li>}
+                                {blog && <li>
+                                    <strong>Website: </strong> {blog}
+                                </li>}
+                            </ul>
+                            <div className="badge badge-primary">Подписчики: {followers}</div>
+                            <div className="badge badge-success">Подписан: {following}</div>
+                            <div className="badge badge-info">Репозитории: {public_repos}</div>
+                            <div className="badge badge-dark">Gists: {public_gists}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Repos repos={repos}/>
+        </>
+    );
+};
